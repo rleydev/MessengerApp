be4c17535ea97f4755a71dc254dd447903ef9fb5
Arthur Lee <archerlv99@gmail.com>
2022-08-09 10:12:40 +0600

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Arthur Lee on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
