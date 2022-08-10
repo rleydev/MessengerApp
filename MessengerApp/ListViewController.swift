6d09edcf119e2f6151c9ec505d5bae01d2099c64
Arthur Lee <archerlv99@gmail.com>
2022-08-10 10:54:12 +0600

    UICollectionViewDiffableDataSource in ListViewController done instead collectionview data source

--- MessengerApp/ListViewController.swift
+++ MessengerApp/ListViewController.swift
@@ -7,18 +7,48 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
 
-    private lazy var collectionView = UICollectionView()
+struct MChat: Hashable {
+    var username: String
+    var userImage: UIImage
+    var lastMessage: String
+    var id = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MChat, rhs: MChat) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+fileprivate enum Section: Int, CaseIterable {
+    case activeChats
+}
+
+class ListViewController: UIViewController {
+    
+    private let activeChats: [MChat] = [
+        MChat(username: "Ann", userImage: UIImage(named: "human1")!, lastMessage: "How are you?"),
+        MChat(username: "Bob", userImage: UIImage(named: "human2")!, lastMessage: "How are you?"),
+        MChat(username: "Est", userImage: UIImage(named: "human3")!, lastMessage: "How are you?"),
+        MChat(username: "Mila", userImage: UIImage(named: "human4")!, lastMessage: "How are you?")
+    ]
+    
+    fileprivate var dataSource: UICollectionViewDiffableDataSource<Section, MChat>?
+    private var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUpCollectionView()
-        setUpSearchBar()
+        
+        setupSearchBar()
+        setupCollectionView()
+        createDataSource()
+        reloadData()
     }
     
-    private func setUpSearchBar() {
+    private func setupSearchBar() {
         navigationController?.navigationBar.barTintColor = .mainWhite()
         navigationController?.navigationBar.shadowImage = UIImage()
         let searchController = UISearchController(searchResultsController: nil)
@@ -28,63 +58,66 @@ class ListViewController: UIViewController {
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
     }
-
-    private func setUpCollectionView() {
+    
+    private func setupCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .mainWhite()
-
         view.addSubview(collectionView)
-
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
-
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+    }
+    
+    private func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, MChat>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, chat) -> UICollectionViewCell? in
+            guard let section = Section(rawValue: indexPath.section) else {
+                fatalError("Unknown section kind")
+            }
+            
+            switch section {
+            case .activeChats:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+                cell.backgroundColor = .gray
+                return cell
+            }
+        })
+    }
+    
+    private func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, MChat>()
+        snapshot.appendSections([.activeChats])
+        snapshot.appendItems(activeChats, toSection: .activeChats)
+        dataSource?.apply(snapshot, animatingDifferences: true)
     }
     
     private func createCompositionalLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+        let layout = UICollectionViewCompositionalLayout { (senctionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
             
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: .fractionalHeight(1))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(84))
-            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .absolute(84))
             let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-            
-            group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 8, trailing: 8)
+            group.contentInsets = NSDirectionalEdgeInsets.init(top: 0, leading: 0, bottom: 8, trailing: 0)
             
             let section = NSCollectionLayoutSection(group: group)
-            section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
-
+            section.contentInsets = NSDirectionalEdgeInsets.init(top: 16, leading: 20, bottom: 0, trailing: 20)
             return section
         }
-        
         return layout
     }
 }
 
-extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = .gray
-        cell.layer.borderWidth = 1
-        return cell
-    }
-    
-}
-
+// MARK: - UISearchBarDelegate
 extension ListViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    private func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
     }
 }
+
 
 import SwiftUI
 
