f1286606a08ccae12660bbd968031afae0c6e1ab
Arthur Lee <archerlv99@gmail.com>
2022-08-10 14:55:10 +0600

    waiting chat cell configured

---
+++ MessengerApp/Cell/WaitingChatCell.swift
@@ -0,0 +1,68 @@
+//
+//  WaitingChatCell.swift
+//  MessengerApp
+//
+//  Created by Arthur Lee on 10.08.2022.
+//
+
+import UIKit
+
+class WaitingChatCell: UICollectionViewCell, SelfConfiguringCell {
+    
+    static var reuseId = "WaitingChatCell"
+    
+    let friendImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.layer.cornerRadius = 10
+        clipsToBounds = true
+        setUpConstraints()
+    }
+    
+    func configure(with value: MChat) {
+        friendImageView.image = UIImage(named: value.userImageString)
+    }
+    
+    private func setUpConstraints() {
+        
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(friendImageView)
+        
+        NSLayoutConstraint.activate([
+            friendImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            friendImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            friendImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+import SwiftUI
+
+struct WaitingChatProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let tabBarVC = MainTabBarController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<WaitingChatProvider.ContainerView>) -> MainTabBarController {
+            return tabBarVC
+        }
+        
+        func updateUIViewController(_ uiViewController: WaitingChatProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<WaitingChatProvider.ContainerView>) {
+            
+        }
+    }
+}
+
