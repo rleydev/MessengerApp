19ab63808d9afb47b4a2d98a8d69b81d5d82620c
Arthur Lee <archerlv99@gmail.com>
2022-08-10 12:26:32 +0600

    active cell done and mock data set

---
+++ MessengerApp/Cell/ActiveChatCell.swift
@@ -0,0 +1,103 @@
+//
+//  ActiveChatCell.swift
+//  MessengerApp
+//
+//  Created by Arthur Lee on 10.08.2022.
+//
+
+import UIKit
+
+protocol SelfConfiguringCell {
+    static var reuseId: String { get }
+    func configure(with value: MChat)
+}
+
+class ActiveChatCell: UICollectionViewCell, SelfConfiguringCell {
+    
+    static var reuseId: String = "ActiveCell"
+    let friendImageView = UIImageView()
+    let friendName = UILabel(text: "User Name", font: .laoSangnamMN20())
+    let lastMessage = UILabel(text: "whatsup?", font: .laoSangnamMN18())
+    let gradientView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        setUpConstraints()
+        
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+    func configure(with value: MChat) {
+        friendImageView.image = UIImage(named: value.userImageString)
+        friendName.text = value.username
+        lastMessage.text = value.lastMessage
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ActiveChatCell {
+    
+    private func setUpConstraints() {
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        friendName.translatesAutoresizingMaskIntoConstraints = false
+        lastMessage.translatesAutoresizingMaskIntoConstraints = false
+        
+        friendImageView.backgroundColor = .gray
+        gradientView.backgroundColor = .blue
+        
+        addSubview(friendImageView)
+        addSubview(gradientView)
+        addSubview(friendName)
+        addSubview(lastMessage)
+        
+        NSLayoutConstraint.activate([
+            friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            friendImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            friendImageView.heightAnchor.constraint(equalToConstant: 78),
+            friendImageView.widthAnchor.constraint(equalToConstant: 78),
+            
+            gradientView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            gradientView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            gradientView.heightAnchor.constraint(equalToConstant: 78),
+            gradientView.widthAnchor.constraint(equalToConstant: 8),
+            
+            friendName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            friendName.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            friendName.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16),
+            
+            lastMessage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            lastMessage.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            lastMessage.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16)
+        ])
+    }
+    
+}
+
+import SwiftUI
+
+struct ActiveChatProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let tabBarVC = MainTabBarController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) -> MainTabBarController {
+            return tabBarVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ActiveChatProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) {
+            
+        }
+    }
+}
--- MessengerApp/Core/Extensions/UIKitExtensions/UIFont+Extension.swift
+++ MessengerApp/Core/Extensions/UIKitExtensions/UIFont+Extension.swift
@@ -17,4 +17,12 @@ extension UIFont {
         return UIFont.init(name: "avenir", size: 26)
     }
     
+    static func laoSangnamMN20() -> UIFont? {
+        return UIFont.init(name: "Lao Sangnam MN", size: 20)
+    }
+    
+    static func laoSangnamMN18() -> UIFont? {
+        return UIFont.init(name: "Lao Sangnam MN", size: 18)
+    }
+    
 }
